7c5d82da75a1f2290c906c9aea683c6ad9106f65
Carlos Silva <clsilva78@gmail.com>
2023-10-30 14:00:01 -0500

    Refactor: import toast css styles into site-globals.css

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,34 +1,14 @@
 import { Footer } from '@ui/components/Footer'
 import { ToastContainer } from '@ui/components/ToastContainer'
+import { mono, sans, title } from '@ui/fonts'
 import '@ui/styles/site-globals.css'
 import type { Metadata } from 'next'
-import { Fira_Code, Nunito, Roboto } from 'next/font/google'
 import type { ReactNode } from 'react'
-import 'react-toastify/dist/ReactToastify.min.css'
 
 export const metadata: Metadata = {
   title: 'Carlos Silva',
   description: 'Frontend developer & Firmware developer.'
 }
-
-const title = Roboto({
-  weight: ['700'],
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-title'
-})
-
-const sans = Nunito({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-sans'
-})
-
-const mono = Fira_Code({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-mono'
-})
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
