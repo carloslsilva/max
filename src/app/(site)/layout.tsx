7c0e994236e784eabe36b4bb0d082e663f582838
Carlos Silva <clsilva78@gmail.com>
2023-12-29 14:11:35 -0500

    Fonts code move to into site-layout

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,10 +1,29 @@
 import { Footer } from '@/components/Footer'
 import { ToastContainer } from '@/components/ToastContainer'
-import { mono, sans, title } from '@/fonts'
 import '@/styles/site-globals.css'
 import { Analytics } from '@vercel/analytics/react'
 import type { Metadata } from 'next'
+import { Fira_Code, Nunito, Roboto } from 'next/font/google'
 import type { ReactNode } from 'react'
+
+const title = Roboto({
+  weight: ['700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-title'
+})
+
+const sans = Nunito({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-sans'
+})
+
+const mono = Fira_Code({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-mono'
+})
 
 export const metadata: Metadata = {
   title: 'Carlos Silva',
