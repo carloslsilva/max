c1320b35d6efbcae00c59c1bc67dbd7aa525f7e5
Carlos Silva <clsilva78@gmail.com>
2024-01-30 14:23:09 -0500

    Update packages

--- src/sanity/sanity.config.ts
+++ src/sanity/sanity.config.ts
@@ -1,12 +1,38 @@
 import { visionTool } from '@sanity/vision'
 import { defineConfig } from 'sanity'
 import { markdownSchema } from 'sanity-plugin-markdown'
-import { deskTool } from 'sanity/desk'
-import { deskStructure } from './deskStructure'
+import type { StructureResolver } from 'sanity/structure'
+import { structureTool } from 'sanity/structure'
 import { schemaTypes } from './schema'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
+
+const structureResolver: StructureResolver = S =>
+  S.list()
+    .title('Content')
+    .items([
+      // Blog related
+      S.listItem()
+        .title('Post')
+        .child(S.documentTypeList('post').title('post')),
+      S.listItem()
+        .title('Category')
+        .child(S.documentTypeList('category').title('category')),
+      S.listItem()
+        .title('Author')
+        .child(S.documentTypeList('author').title('author')),
+      // Divider
+      S.divider(),
+      // Experience/Landing related
+      S.listItem().title('Job').child(S.documentTypeList('job').title('job')),
+      S.listItem()
+        .title('Project')
+        .child(S.documentTypeList('project').title('project')),
+      S.listItem()
+        .title('Skill')
+        .child(S.documentTypeList('skill').title('skill'))
+    ])
 
 export const config = defineConfig({
   name: 'default',
@@ -15,8 +41,8 @@ export const config = defineConfig({
   dataset,
   basePath: '/admin',
   plugins: [
-    deskTool({
-      structure: deskStructure
+    structureTool({
+      structure: structureResolver
     }),
     visionTool(),
     markdownSchema()
