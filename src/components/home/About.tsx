58e567d9b837c97b0bb884f4bc9006d4352dc572
Carlos Silva <clsilva78@gmail.com>
2024-02-13 02:26:15 -0500

    Changes into about section styles

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -12,7 +12,7 @@ type Props = {
 
 export const About: FC<Props> = ({ bio }) => (
   <Section>
-    <div className='mt-16 flex flex-col items-center gap-10 md:flex-row md:gap-10 lg:gap-24'>
+    <div className='mt-16 flex flex-col items-center gap-10 md:flex-row lg:gap-24'>
       <div className='w-5/6 md:w-4/6 lg:w-full lg:max-w-lg'>
         <Image
           className='rounded-xl object-cover object-center shadow-2xl'
